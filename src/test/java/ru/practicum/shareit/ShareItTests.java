568760259c57c89efe4c52047bc40067bc7e0181
Nazar Shmigelskiy <nazarshmigelskiy@yandex.ru>
2026-09-12 18:09:01 +0300

    Checkstyle err fix

--- src/test/java/ru/practicum/shareit/ShareItTests.java
+++ src/test/java/ru/practicum/shareit/ShareItTests.java
@@ -3,12 +3,13 @@ package ru.practicum.shareit;
 import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+
 @Disabled
 @SpringBootTest
 class ShareItTests {
 
-	@Test
-	void contextLoads() {
-	}
+    @Test
+    void contextLoads() {
+    }
 
 }
